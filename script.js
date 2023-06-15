7722a21caa004ef15c70b46dd8a75755f7b4f893
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-06-14 23:31:31 -0600

    Create simple functions

---
+++ script.js
@@ -0,0 +1,18 @@
+// Create functions for basic math operatos
+// add, subtract, multiply, divide 
+
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
