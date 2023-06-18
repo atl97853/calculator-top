023c1caaf79ce270fb72adb1ef19a03a52b93df2
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-06-17 21:43:43 -0600

    Create first version of operator() function

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+// Create operator function 
+// takes two numbers, an operator and returns the result 
+
+function operator(numA, numB, operator) {
+    switch (operator) {
+        case '+':
+            return add(numA, numB);
+        case '-':
+            return subtract(numA, numB);
+        case '*':
+            return multiply(numA, numB);
+        case '/':
+            return divide(numA, numB);
+    };
+};
